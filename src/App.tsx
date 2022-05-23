bccf54ede6c08a17952f3b6edffab8cdcabe1341
TAKAHASHI Taro <takahashi.taro.keitai@gmail.com>
2022-05-23 17:28:26 +0900

    React 18対応

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { useState, VFC } from 'react';
+import { useState, FC } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App: VFC = () => {
+const App: FC = () => {
   const [count, setCount] = useState(0);
 
   return (
